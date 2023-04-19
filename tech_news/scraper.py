3eaa5e2e528fcdbf0990e3724a77b92ccd0cc1da
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-04-19 00:29:27 -0300

    requisito 4 realizado

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -37,7 +37,19 @@ def scrape_next_page_link(html_content):
 
 
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    return {
+        'url': selector.css('link[rel=canonical]::attr(href)').get(),
+        'title': selector.css('h1.entry-title::text').get().strip(),
+        'timestamp': selector.css('li.meta-date::text').get(),
+        'writer': selector.css('a.url.fn::text').get(),
+        'reading_time': int(
+            selector.css('li.meta-reading-time::text').get().split(" ")[0]
+        ),
+        'summary': selector.css(".entry-content p")
+        .xpath("string()").get().strip(),
+        'category': selector.css('.meta-category .label::text').get(),
+    }
 
 
 # Requisito 5
