ec722c56679642e6cb30c36e243bc3e828a3c0fd
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-04-19 00:50:11 -0300

    requisito 10 realizado

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,12 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    return [
+        category[0]
+        for category in Counter(
+            [news["category"] for news in find_news()]
+        ).most_common(5)
+    ]
